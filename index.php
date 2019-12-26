ed8b8d8ab9174de52255b4b9973a9d189bdd6935
altayevrim <evrimaltay@gmail.com>
2019-12-26 06:41:56 +0200

    new index.php file for testing

--- index.php
+++ index.php
@@ -11,11 +11,20 @@
                 'pass' => 'toor'
             ],
             'freepass' => [
-                '127.0.0.1'
+                // '127.0.0.1'
             ]
         ]
     );
-    // echo $rimsec->getIP();
-    $rimsec->addBan(12);
+
+    $rimsec->template('rimsec.html','html');
     
+    $rimsec->checkBan();
+    // echo $rimsec->getIP();
+    if(isset($_GET['bruteforce'])){
+        $rimsec->addBan(10, 0, '+1 day', 'He is ugly.');
+        echo '<h1>Bruteforced</h1>';
+        exit;
+    }
+
+
     echo '<h1>You are free to go!</h1>';
